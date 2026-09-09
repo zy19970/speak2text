2d96b3b3931dfa45a7100cd11f22cf25591cda26
InTron <zy19970@hotmail.com>
2026-09-09 14:34:35 +0800

    增加CPU和GPU资源限制选项

--- src/Speak2Text/Models/TranscriptionOptions.cs
+++ src/Speak2Text/Models/TranscriptionOptions.cs
@@ -11,4 +11,22 @@ public sealed class TranscriptionOptions
     public bool ExportText { get; init; } = true;
     public bool ExportSrt { get; init; } = true;
     public bool ExportJson { get; init; } = true;
+
+    public bool LimitCpu { get; init; } = true;
+    public int MaxCpuPercent { get; init; } = 70;
+    public bool LimitGpu { get; init; } = true;
+    public int MaxGpuPercent { get; init; } = 70;
+
+    public int CpuThreadLimit
+    {
+        get
+        {
+            if (!LimitCpu)
+                return 0;
+
+            var logicalProcessors = Math.Max(1, Environment.ProcessorCount);
+            var percent = Math.Clamp(MaxCpuPercent, 10, 100);
+            return Math.Max(1, (int)Math.Floor(logicalProcessors * percent / 100d));
+        }
+    }
 }
