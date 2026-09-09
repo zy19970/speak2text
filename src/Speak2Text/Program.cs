bf6b1d8a22e4718825ecf853e328e199b81907ff
InTron <zy19970@hotmail.com>
2026-09-09 11:48:51 +0800

    添加 WinForms 项目基础结构

---
+++ src/Speak2Text/Program.cs
@@ -0,0 +1,11 @@
+namespace Speak2Text;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
